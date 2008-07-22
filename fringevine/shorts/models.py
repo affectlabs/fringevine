10afee7f24c5493659e38b6256db0ef548082150
Jennie <jennie@affectlabs.com>
2008-07-22 14:39:33 +0100

    further model refinement

--- fringevine/shorts/models.py
+++ fringevine/shorts/models.py
@@ -1,27 +1,67 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
    
 class Company(models.Model):
    name = models.CharField(max_length=255)
+   class Admin:
+      pass
+   def __unicode__(self):
+      return self.name
 
 class Genre(models.Model):
    name = models.CharField(max_length=255)
+   class Admin:
+      pass
+   def __unicode__(self):
+      return self.name
 
 class Venue(models.Model):
    name = models.CharField(max_length=255)
-   location = models.CharField(max_length=255)
-   gridref = models.CharField(max_length=10)
-   phone = models.IntegerField()
-   edfringe_id = models.IntegerField()
+   location = models.CharField(max_length=255, blank=True, null=True)
+   gridref = models.CharField(max_length=10,blank=True, null=True)
+   phone = models.IntegerField(blank=True, null=True)
+   edfringe_id = models.IntegerField(blank=True, null=True)
+   class Admin:
+      pass
+   def __unicode__(self):
+      return self.name
 
 class Show(models.Model):
    name = models.CharField(max_length=255)
    company = models.ForeignKey(Company)
    genre = models.ForeignKey(Genre)
    venue = models.ForeignKey(Venue)
-   desc = models.TextField('Blurb')
-   date_from = models.DateTimeField('Start date')
-   date_to   = models.DateTimeField('End date')
+   desc = models.TextField('Blurb', blank=True, null=True)
+   date_from = models.DateTimeField('Start date', blank=True, null=True)
+   date_to   = models.DateTimeField('End date', blank=True, null=True)
    edfringe_id = models.IntegerField()
    thumbsup = models.DecimalField(max_digits=5,decimal_places=3)
+   class Admin:
+      pass
+   def __unicode__(self):
+      return self.name
+
+class Author(models.Model):
+   name = models.CharField(max_length=255)
+   IP = models.IPAddressField(blank=True, null=True)
+   login = models.CharField(max_length=50, blank=True, null=True)
+   pass_hash = models.CharField('Password hash',max_length=50,blank=True, null=True)
+   source = models.CharField(max_length=50, blank=True, null=True)
+   url = models.CharField(max_length=255, blank=True, null=True)
+   sms = models.IntegerField(blank=True, null=True)
+   class Admin:
+      pass
+   def __unicode__(self):
+      return self.name
+
+class Short(models.Model):
+   author = models.ForeignKey(Author)
+   text = models.CharField(max_length=200)
+   show = models.ForeignKey(Show)
+   source = models.CharField(max_length=255, blank=True, null=True)
+   date = models.DateTimeField(default=datetime.now)
+   thumbsup = models.DecimalField(max_digits=5,decimal_places=3)
+   class Admin: 
+      pass
